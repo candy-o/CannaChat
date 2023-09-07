26b6fee680e150aada3248e74930aaf00cd9ca9b
Candace O'Sullivan-Sutherland <54730118+candy-o@users.noreply.github.com>
2023-09-07 11:39:19 -0400

    Update cannachat_app.py
    fixed typo

--- cannachat_app.py
+++ cannachat_app.py
@@ -1,3 +1,3 @@
 import streamlit as st
 
-st.write('Welcome to CannaCHAT! ROCK YOUR DAY! (in development - placeholder)'
+st.write('Welcome to CannaCHAT! ROCK YOUR DAY! (in development - placeholder')
